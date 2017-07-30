4748f2575ff360986128cb86b33b9b662d4ce6dc
devnirajc <nirajd327@gmail.com>
2017-07-30 11:15:51 +0530

    unit converter code added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -34,12 +34,70 @@ export class AppComponent {
     onSubmit = function(user){
         console.log(user);
     }
+    // form;
+    // ngOnInit(){
+    //     this.form = new FormGroup({
+    //         firstname: new FormControl("Niraj"),
+    //         lastname: new FormControl(""),
+    //         languages: new FormControl("")
+    //     });
+    // } 
+
     form;
     ngOnInit(){
         this.form = new FormGroup({
-            firstname: new FormControl("Niraj"),
-            lastname: new FormControl(""),
-            languages: new FormControl("")
+            decimal: new FormControl(""),
+            hexa: new FormControl(""),
+            binary: new FormControl(""),
+            octal: new FormControl("")
         });
-    } 
+    }
+    decimalChanged = function(oldvalue, newvalue){
+        if(newvalue != ""){
+            this.form.patchValue({binary: parseInt(newvalue, 10).toString(2)});  
+            this.form.patchValue({octal: parseInt(newvalue, 10).toString(8)});
+            this.form.patchValue({hexa: parseInt(newvalue, 10).toString(16)});        
+        } else{
+            this.form.patchValue({binary: ""});
+            this.form.patchValue({octal: ""});
+            this.form.patchValue({hexa: ""});
+        }
+    }
+
+    b = 0;
+    o = 0;
+    h = 0;
+    binaryChanged = function(oldvalue, newvalue) {
+        this.b = this.b + 1;
+        if(this.b == 1) {
+            if(newvalue != ""){
+            this.form.patchValue({decimal: parseInt(newvalue, 2).toString(10)});       
+            } else {
+                this.form.patchValue({decimal: ""});
+            }
+            this.b = 0;
+        }
+    }
+    octalChanged = function(oldvalue, newvalue) {
+        this.o = this.o + 1;
+        if(this.o == 1) {
+            if(newvalue != ""){
+            this.form.patchValue({decimal: parseInt(newvalue, 8).toString(10)});       
+            } else {
+                this.form.patchValue({decimal: ""});
+            }
+            this.b = 0;
+        }
+    }
+    hexaChanged = function(oldvalue, newvalue) {
+        this.h = this.h + 1;
+        if(this.h == 1) {
+            if(newvalue != ""){
+            this.form.patchValue({decimal: parseInt(newvalue, 16).toString(10)});       
+            } else {
+                this.form.patchValue({decimal: ""});
+            }
+            this.b = 0;
+        }
+    }
 }
